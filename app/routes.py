957a38eb4809f8384be3b1676837139e8f851aed
classidope <classidso2@gmail.com>
2024-09-25 13:34:39 +0530

    changed routes.py, styles.css and run.py. Added static mp3 file and add_song.html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app, connect_to_db
 from flask import render_template, request
+from flask import flash
 
 def get_default_moods():
     conn = connect_to_db()
@@ -40,3 +41,30 @@ def get_mood():
 
 
     return render_template('results.html', data=data, Mood=mood)
+
+@app.route('/add-song', methods=['GET','POST'])
+def add_song():
+    if request.method == "POST":
+        artist = request.form.get('artist')
+        title = request.form.get('title')
+        mood = request.form.get('mood')
+
+
+        conn = connect_to_db()
+        cursor = conn.cursor()
+
+        try:
+            query = "INSERT INTO songs (artist, title, mood) VALUES (%s, %s, %s)"
+            cursor.execute(query, (artist, title, mood))
+            conn.commit()
+            flash('Your DAMN Song added successfully!')
+
+            cursor.close()
+            conn.close()
+
+        except Exception as e:
+            conn.rollback()
+            flash('Failed to add song, cuz it was TRASH. js ;) Try again later, motherfucker')
+
+    return render_template('add_song.html')
+
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import app
+import os
+
+app.secret_key = os.urandom(24)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
